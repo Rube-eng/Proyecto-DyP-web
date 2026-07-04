833ddaa6fce8fcc3237fe1078967a07095da46b1
Rube-eng <rfallasl@ucenfotec.ac.cr>
2026-07-04 01:18:10 -0600

    primerosAvancesProjecto

---
+++ js/registro-manual.egresados.js
@@ -0,0 +1,10 @@
+/* seleccionar los elementos del DOM */
+const inputIdentificacion = document.getElementById("identificacion")
+const inputNombre = document.getElementById("nombre-completo")
+
+// correo
+const inputCorreo = document.getElementById("correo")
+const inputTelefono = document.getElementById("telefono")
+const inputFechaRegistro = document.getElementById("fecha-registro")
+
+const btnRegistrarEgresado = document.getElementById("guardar-egresado")
